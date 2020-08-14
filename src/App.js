d1b082ea5318958253ed3cabf8fe12bd6440b612
vjeuel <vjeuel@gmail.com>
2020-08-14 17:24:16 -0500

    trying to fix pics address

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Route } from "react-router-dom";
 import Directory from "./pages/Directory"
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <div className="App-header">
-          <Route exact path="/" component={Directory} />
-        </div>
-      </div>
-    </Router>
+    <Directory />
   );
 }
 
