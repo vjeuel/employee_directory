2af554f96e55bf0cebed0a08bd410738bc10bfff
vjeuel <vjeuel@gmail.com>
2020-08-15 18:21:45 -0500

    Henry's solution for the pictures

--- src/components/EmployeeRow/index.js
+++ src/components/EmployeeRow/index.js
@@ -2,10 +2,19 @@ import React from "react";
 import "./style.css";
 
 function EmployeeRow(props) {
+   const getImage = (id) => {
+      switch (id) {
+         case 1: return require("./images/Riley_Moore.jpg");
+         default: return "https://via.placeholder.com/150";
+         case 2: return require("./images/Erin_Anderson.jpg");
+         default: return "https://via.placeholder.com/150";
+      }
+   }
+
    return (
       <tr className="employeeRow">
          <td>
-            <img src={props.photo} alt={props.name} />
+            <img src={getImage(props.id)} alt={props.name} />
          </td>
          <td>{props.name}</td>
          <td>{props.phone_number}</td>
@@ -13,6 +22,7 @@ function EmployeeRow(props) {
          <td>{props.dob}</td>
       </tr>
    );
+
 };
 
 export default EmployeeRow;
